e198bac816c0509cb37aca1e566fd723d92770e8
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-07-27 18:59:24 -0700

    clean up gservice

--- app/routes.js
+++ app/routes.js
@@ -5,22 +5,25 @@ module.exports = function(app) {
 
   app.get('/users', function(req, res) {
     var query = User.find({});
-    query.exec(function(err, users) {
-      if (err)
+    query.exec(function(err, users){
+      if (err) {
         res.send(err);
+      } else {
 
       res.json(users);
+      }
     });
   });
 
-  app.post('/users', function(req, res) {
+  app.post('/users', function(req, res){
     var newuser = new User(req.body);
 
     newuser.save(function(err) {
       if (err)
         res.send(err);
+      else
 
-      res.json(req.body);
+        res.json(req.body);
     });
   });
 };
--- public/js/addCtrl.js
+++ public/js/addCtrl.js
@@ -1,34 +1,81 @@
-var addCtrl = angular.module('addCtrl', ['geolocation']);
-addCtrl.controller('addCtrl', function($scope, $http, geolocation){
+var addCtrl = angular.module('addCtrl', ['geolocation', 'gservice']);
+addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, gservice) {
+
   $scope.formData = {};
-    var coords = {};
-    var lat = 0;
-    var long = 0;
+  var coords = {};
+  var lat = 0;
+  var long = 0;
 
-    $scope.formData.latitude = 39.500;
-    $scope.formData.longitude = -98.350;
+  $scope.formData.longitude = -98.350;
+  $scope.formData.latitude = 39.500;
 
-    $scope.createUser = function() {
-      var userData = {
-          username: $scope.formData.username,
-          gender: $scope.formData.gender,
-          age: $scope.formData.age,
-          favlang: $scope.formData.favlang,
-          location: [$scope.formData.longitude, $scope.formData.latitude],
-          htmlverified: $scope.formData.htmlverified
+  geolocation.getLocation().then(function(data) {
+
+
+    coords = {
+      lat: data.coords.latitude,
+      long: data.coords.longitude
+    };
+
+
+    $scope.formData.longitude = parseFloat(coords.long).toFixed(3);
+    $scope.formData.latitude = parseFloat(coords.lat).toFixed(3);
+
+
+    $scope.formData.htmlverified = "Yep (real data!)";
+
+    gservice.refresh($scope.formData.latitude, $scope.formData.longitude);
+
+  });
+
+  $rootScope.$on("clicked", function() {
+
+
+    $scope.$apply(function() {
+      $scope.formData.latitude = parseFloat(gservice.clickLat).toFixed(3);
+      $scope.formData.longitude = parseFloat(gservice.clickLong).toFixed(3);
+      $scope.formData.htmlverified = "Nope (Thanks for literaly nothing)";
+    });
+  });
+
+  $scope.refreshLoc = function() {
+    geolocation.getLocation().then(function(data) {
+      coords = {
+        lat: data.coords.latitude,
+        long: data.coords.longitude
       };
 
-      $http.post('/users', userData)
-            .success(function (data) {
+      $scope.formData.longitude = parseFloat(coords.long).toFixed(3);
+      $scope.formData.latitude = parseFloat(coords.lat).toFixed(3);
+      $scope.formData.htmlverified = "Yep (Thanks for giving us real data!)";
+      gservice.refresh(coords.lat, coords.long);
+    });
+  };
 
-              $scope.formData.username = "";
-              $scope.formData.gender = "";
-              $scope.formData.age = "";
-              $scope.formData.favlang = "";
+  $scope.createUser = function() {
+    var userData = {
+      username: $scope.formData.username,
+      gender: $scope.formData.gender,
+      age: $scope.formData.age,
+      favlang: $scope.formData.favlang,
+      location: [$scope.formData.longitude, $scope.formData.latitude],
+      htmlverified: $scope.formData.htmlverified
+    };
 
-            })
-         .error(function (data) {
-             console.log('Error: ' + data);
-         });
- };
+    $http.post('/users', userData)
+      .success(function(data) {
+
+        $scope.formData.username = "";
+        $scope.formData.gender = "";
+        $scope.formData.age = "";
+        $scope.formData.favlang = "";
+
+        gservice.refresh($scope.formData.latitude, $scope.formData.longitude);
+      })
+
+    .error(function(data) {
+      console.log('Error: ' + data);
+    });
+
+  };
 });
