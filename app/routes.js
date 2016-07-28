3b083cb2ee1c1e78d2fbd75e17c7b0d4acf7e588
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-07-28 13:51:42 -0700

    add all of query filters

--- app/routes.js
+++ app/routes.js
@@ -30,6 +30,13 @@ module.exports = function(app) {
     var lat = req.body.latitude;
     var long = req.body.longitude;
     var distance = req.body.distance;
+    var male            = req.body.male;
+    var female          = req.body.female;
+    var other           = req.body.other;
+    var minAge          = req.body.minAge;
+    var maxAge          = req.body.maxAge;
+    var favLang         = req.body.favlang;
+    var reqVerified     = req.body.reqVerified;
 
     var query = User.find({});
 
@@ -38,6 +45,26 @@ module.exports = function(app) {
 
       maxDistance: distance * 1609.34, spherical: true});
 
+    }
+
+    if(male || female || other){
+        query.or([{ 'gender': male }, { 'gender': female }, {'gender': other}]);
+    }
+
+    if(minAge){
+        query = query.where('age').gte(minAge);
+    }
+
+    if(maxAge){
+        query = query.where('age').lte(maxAge);
+    }
+
+    if(favLang){
+        query = query.where('favlang').equals(favLang);
+    }
+    
+    if(reqVerified){
+        query = query.where('htmlverified').equals("Yep (Thanks for giving us real data!)");
     }
     query.exec(function(err, users){
         if(err)
